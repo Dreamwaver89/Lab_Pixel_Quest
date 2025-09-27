183092fbcb264c66fc87f166fd54bccaaa433af8
Dreamwaver89 <delangelelmeri@gmail.com>
2025-09-27 12:29:51 -0400

    commit

--- Assets/Geo_Quest/Scripts/Geo Controller.cs
+++ Assets/Geo_Quest/Scripts/Geo Controller.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+    string variable = "Hello";
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Hello World");
-        
-    }
+    void Start() {
 
+        Debug.Log("Hello World");
+
+        string variable2 = "World" + variable;
+
+            }
     // Update is called once per frame
     void Update()
-    { 
-      
-        
-    }
-}
+    { }
+
+} 
+    
+
