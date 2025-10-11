3999e6ae55cfebcfce4eab9e83ca487bf878528b
Dreamwaver89 <delangelelmeri@gmail.com>
2025-10-11 12:32:48 -0400

    K

--- Assets/Geo_Quest/Scripts/Geo Controller.cs
+++ Assets/Geo_Quest/Scripts/Geo Controller.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class GeoController : MonoBehaviour
 {
     string variable = "Hello";
+    Rigidbody2D bill;
+    /* 
+     hello bro
+     */
+    public float Dell;
+
+
     // Start is called before the first frame update
     void Start() {
+        bill = GetComponent<Rigidbody2D>();
 
         Debug.Log("Hello World");
 
@@ -15,7 +23,24 @@ public class GeoController : MonoBehaviour
             }
     // Update is called once per frame
     void Update()
-    { }
+    {
+        // bill.velocity = new Vector2(1, bill.velocity.y);
+
+        //yfyfyufgy
+        if (Input.GetKey(KeyCode.D))
+        {
+            bill.velocity = new Vector2(Dell, bill.velocity.y);
+        }
+        else {
+        bill.velocity = new Vector2(0, bill.velocity.y);
+        
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            bill.velocity = new Vector2(-Dell, bill.velocity.y);
+        }
+    
+    }   
 
 } 
     
