2adbab3c988d1f8783e0fcd53c2d4e507ac74f96
Dreamwaver89 <delangelelmeri@gmail.com>
2025-11-01 12:24:46 -0400

    yuhhhhhhhhhhhhhhhhhhhh

--- Assets/Pixel_Quest/Scripts/PlayerJump.cs
+++ Assets/Pixel_Quest/Scripts/PlayerJump.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Comonents
+    private Rigidbody2D _rigidbody2D;
+    // forces
+    public float jumpforce = 10;
+    // Capsule
+    public float CapsuleHeight = 0.25f;
+    public float CapsuleRadius = 0.08f;
+
+
+    // Gound Check
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool _groundCheck;
+
+
+    // Sets gravity vector and connects components
+
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {  // Checks if the player is touching ground
+        _groundCheck = Physics2D.OverlapCapsule(feetCollider.position,
+                new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Horizontal,
+                 0, groundMask);
+        // checks if player is trying to jump/can jump
+        if (Input.GetKeyDown(KeyCode.Space) && _groundCheck)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpforce);
+        }
     }
 }
--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
-{
+
+{ private SpriteRenderer _SpriteRenderer;            //  Controls player image
+    public float xMultiplier = 4;                    //   Controlls player physics
+    private Rigidbody2D _rigidbody2D;                 //  Controls player X speed
     Rigidbody2D bill;
     /* 
      hello bro
      */
     public float Dell;
 
+
     // Start is called before the first frame update
-    void Start()
-    {
+   private void Start(){ 
+    
         bill = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _SpriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    private void Update(){
+          // Get player movement from player button press
         bill.velocity = new Vector2(Dell * Input.GetAxis("Horizontal"), bill.velocity.y);
+        float xMovement = Input.GetAxis("Horizontal");
+
+
+
+        if (xMovement > 0){ _SpriteRenderer.flipX = true; }
+        else if (xMovement < 0) { _SpriteRenderer.flipX = false; }
+
+        // Give the speed to the rigidbody
+        _rigidbody2D.velocity = new Vector2(xMultiplier * xMovement, _rigidbody2D.velocity.y);
     }
 }
