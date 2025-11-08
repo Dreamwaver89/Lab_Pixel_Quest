e8f1ecc5922e51a57ff5ed527bfebfd798a2612e
Dreamwaver89 <delangelelmeri@gmail.com>
2025-11-08 12:14:46 -0500

    piaswygoiusdbgwi

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PlayerStats : MonoBehaviour
 {
     public string NEXT;
+    private int coinCounter = 0;
+    private int _health = 3;
+    private int _maxHealth = 3;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag)
@@ -13,6 +17,20 @@ public class PlayerStats : MonoBehaviour
             case "Enemy":
                 {
                     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    break;
+
+                }
+
+            case "Health":
+                {
+                    _health++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+            case "Coin":
+                {
+                    coinCounter++;
+                    Destroy(collision.gameObject);
                     break;
                 }
             case "Finish":
