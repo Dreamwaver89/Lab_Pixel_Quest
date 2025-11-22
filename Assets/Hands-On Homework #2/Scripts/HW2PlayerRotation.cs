5b4c0f52d28e51124a7acfe68b61bdc8c43972a8
Dreamwaver89 <delangelelmeri@gmail.com>
2025-11-22 14:28:08 -0500

    skihski

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class HW2PlayerRotation : MonoBehaviour
 {
+    private Camera _Camera;
 
+    private string CameraName = "Game_Camera";
+
+    private void Start()
+    {
+        _Camera = GameObject.Find(CameraName);
+    }
 }
